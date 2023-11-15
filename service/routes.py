84f8fbba7d21631e4d864025f814cd40806f6a05
goluash <goluashcinim@gmail.com>
2023-11-15 09:30:28 -0500

    added update acoount part

--- service/routes.py
+++ service/routes.py
@@ -110,9 +110,8 @@ def update_accounts(account_id):
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
     account.deserialize(request.get_json())
     account.update()
-    
-    return account.serialize(), status.HTTP_200_OK
 
+    return account.serialize(), status.HTTP_200_OK
 
 ######################################################################
 # DELETE AN ACCOUNT
