1d04bfe65dd9a0be5e5fc5873c93181abd03be8c
goluash <150834807+goluash@users.noreply.github.com>
2023-11-21 13:24:17 +0800

    Update __init__.py

--- service/__init__.py
+++ service/__init__.py
@@ -14,9 +14,10 @@ from flask_cors import CORS
 # Create Flask application
 app = Flask(__name__)
 app.config.from_object(config)
-talisman = Talisman(app, content_security_policy=[])
+talisman = Talisman(app)
 CORS(app)
 
+talisman.force_https = False
 # Import the routes After the Flask app is created
 # pylint: disable=wrong-import-position, cyclic-import, wrong-import-order
 from service import routes, models  # noqa: F401 E402
